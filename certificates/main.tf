01761d749a1d9528923ba00c930cf332c30c81e6
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2025-02-24 07:56:58 +0530

    Keystore and Truststore Generation

--- certificates/main.tf
+++ certificates/main.tf
@@ -1,3 +1,47 @@
+// Keystore and Truststore passwords
+resource "random_password" "keystore_password" {
+  length           = 16
+  lower            = true
+  numeric          = true
+  special          = true
+  override_special = "-_*"
+  min_special      = 2
+}
+
+resource "random_password" "truststore_password" {
+  length           = 16
+  lower            = true
+  numeric          = true
+  special          = true
+  override_special = "-_*"
+  min_special      = 2
+}
+
+
+resource "kubernetes_secret" "valkey_certificate_passwords" {
+  metadata {
+    name      = "valkey-certificate-passwords"
+    namespace = var.namespace
+
+    labels = {
+      app       = "photoatom"
+      component = "secret"
+    }
+
+    annotations = {
+      "replicator.v1.mittwald.de/replication-allowed" = "true"
+      "replicator.v1.mittwald.de/replication-allowed-namespaces" : "backend,frontend"
+    }
+  }
+
+  data = {
+    "VALKEY_CA_CERT_PASSWORD"   = base64encode(random_password.truststore_password.result)
+    "VALKEY_USER_CERT_PASSWORD" = base64encode(random_password.keystore_password.result)
+  }
+
+  type = "Opaque"
+}
+
 // Certificate Authority to be used with Valkey Cluster
 resource "kubernetes_manifest" "valkey_ca" {
   manifest = {
@@ -92,6 +136,23 @@ resource "kubernetes_manifest" "valkey_certificate" {
         "annotations" = {
           "replicator.v1.mittwald.de/replication-allowed"            = "true"
           "replicator.v1.mittwald.de/replication-allowed-namespaces" = "backend,frontend"
+        }
+      }
+      "keystores" = {
+        "jks" = {
+          "create" : true
+          "passwordSecretRef" : {
+            "name" : "valkey-certificate-passwords"
+            "key" : "VALKEY_CA_CERT_PASSWORD"
+          }
+          "alias" : "valkey"
+        }
+        "pkcs12" : {
+          "create" : true
+          "passwordSecretRef" : {
+            "name" : "valkey-certificate-passwords"
+            "key" : "VALKEY_USER_CERT_PASSWORD"
+          }
         }
       }
       "issuerRef" = {
